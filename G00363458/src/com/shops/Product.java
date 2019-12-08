66629fd2f9a2c13afe1b8a7334c7b869f0ee309e
Shirin <g00363458@gmit.ie>
2019-12-08 16:41:39 +0000

    Partially completed project

---
+++ G00363458/src/com/shops/Product.java
@@ -0,0 +1,40 @@
+package com.shops;
+
+
+public class Product {
+	
+	//class variables
+		float price;
+		int prodId;
+		int storeId;
+		String prodName;
+		
+		//Getters and Setters
+		public float getPrice() {
+			return price;
+		}
+		public void setPrice(float price) {
+			this.price = price;
+		}
+		public int getProdId() {
+			return prodId;
+		}
+		public void setProdId(int prodId) {
+			this.prodId = prodId;
+		}
+		public int getStoreId() {
+			return storeId;
+		}
+		public void setStoreId(int storeId) {
+			this.storeId = storeId;
+		}
+		public String getProdName() {
+			return prodName;
+		}
+		public void setProdName(String prodName) {
+			this.prodName = prodName;
+		}
+	
+		
+
+}
---
+++ G00363458/src/com/shops/StoreProduct.java
@@ -0,0 +1,55 @@
+package com.shops;
+
+public class StoreProduct {
+	
+	//class variables
+		int storeId;
+		String storeName;
+		String founded;
+		float price;
+		int prodId;
+		String prodName;
+		
+		
+		//Getters and setters
+		public int getStoreId() {
+			return storeId;
+		}
+		public void setStoreId(int storeId) {
+			this.storeId = storeId;
+		}
+		public String getStoreName() {
+			return storeName;
+		}
+		public void setStoreName(String storeName) {
+			this.storeName = storeName;
+		}
+		public String getFounded() {
+			return founded;
+		}
+		public void setFounded(String founded) {
+			this.founded = founded;
+		}
+		public float getPrice() {
+			return price;
+		}
+		public void setPrice(float price) {
+			this.price = price;
+		}
+		public int getProdId() {
+			return prodId;
+		}
+		public void setProdId(int prodId) {
+			this.prodId = prodId;
+		}
+		public String getProdName() {
+			return prodName;
+		}
+		public void setProdName(String prodName) {
+			this.prodName = prodName;
+		}
+				
+		
+
+		
+}
